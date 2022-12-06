7490655ab18f6732e9f1df786b46952da1108a3a
Moyrasc <noemisolac@gmail.com>
2022-12-06 12:40:53 +0100

    created Model, Controller and Migration Likes

--- database/migrations/2022_12_06_113029_create_likes_table.php
+++ database/migrations/2022_12_06_113029_create_likes_table.php
@@ -13,11 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('comentarios', function (Blueprint $table) {
+        Schema::create('likes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('post_id')->constrained()->onDelete('cascade');
-            $table->string('comentario');
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comentarios');
+        Schema::dropIfExists('likes');
     }
 };
