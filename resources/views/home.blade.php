d5872a912e4051d55bea6d88b91a477fb73c6cd9
Moyrasc <noemisolac@gmail.com>
2022-12-07 14:10:49 +0100

    working with components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,22 +5,6 @@
 @endsection
 
 @section('contenido')
-    @if($posts->count())
-            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-    @foreach ($posts as $post)
-    <div>
-        <a href="{{route('posts.show',['post'=> $post, 'user'=>$post->user])}}">
-            <img src="{{ asset('uploads'). '/'. $post->imagen}}" alt="Imagen del post {{$post->titulo}}"/>
-        </a>
-    </div>    
-    @endforeach
-    </div>
-    <div class="my-10">
-        {{$posts->links()}}
-    </div>
-
-    @else
-        <p class="text-center">No hay publicaciones, sigue a alguien para poder mostrar sus publicaciones</p>
-    @endif
+    <x-listar-post :posts="$posts"/>
 @endsection
 
